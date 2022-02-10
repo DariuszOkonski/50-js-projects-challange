495a83c8a29e9e8b584c75ba6398fbf542f70269
Dariusz Okonski <darek200180@gmail.com>
2022-02-10 00:53:17 +0000

    finish project 1 expanding cards

--- day-01-expanding-cards/app.js
+++ day-01-expanding-cards/app.js
@@ -1,5 +1,5 @@
 const containerBlocks = document.querySelectorAll('.container__block');
-
+let cleanActiveWhenSmall = false;
 
 containerBlocks.forEach(element => {
     element.addEventListener('click', () => {
@@ -17,3 +17,19 @@ function removeClassAcitve() {
     })
 }
 
+window.addEventListener('resize', (event) => {
+    if(window.innerWidth <= 768 && !cleanActiveWhenSmall) {
+        cleanActiveWhenSmall = true;
+    } else if(window.innerWidth > 768 && cleanActiveWhenSmall) {
+        cleanActiveWhenSmall = false;
+    }
+
+    if(cleanActiveWhenSmall & !containerBlocks[0].classList.contains('container__block--active')) {
+        if(containerBlocks[1].classList.contains('container__block--active') || containerBlocks[2].classList.contains('container__block--active'))
+            return;
+        
+        removeClassAcitve();
+        containerBlocks[0].classList.add('container__block--active');        
+    }
+
+}, true);
