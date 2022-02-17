5d1f2167f7f9f9c6fdffbf8bcf3b551e57e494e0
Dariusz Okonski <darek200180@gmail.com>
2022-02-17 04:22:19 +0000

    working on styling content page

--- day-03-rotating-navigation/app.js
+++ day-03-rotating-navigation/app.js
@@ -2,13 +2,16 @@ const navigation = document.querySelector('.navigation');
 const navigationSide = document.querySelector('.navigation-side');
 const btnOpen = document.querySelector('.navigation__open');
 const btnClose = document.querySelector('.navigation__close');
+const content = document.querySelector('.content');
 
 btnOpen.addEventListener('click', () => {
     navigation.classList.add('navigation--active');
     navigationSide.classList.add('navigation-side--active');
+    content.classList.add('content--active');
 })
 
 btnClose.addEventListener('click', () => {
     navigation.classList.remove('navigation--active');
     navigationSide.classList.remove('navigation-side--active');
+    content.classList.remove('content--active');
 })
