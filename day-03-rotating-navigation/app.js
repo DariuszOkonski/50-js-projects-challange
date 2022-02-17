3567c92669ecd5e4fb502c910dba5ec9066e0f25
Dariusz Okonski <darek200180@gmail.com>
2022-02-17 04:03:45 +0000

    add side navigation

--- day-03-rotating-navigation/app.js
+++ day-03-rotating-navigation/app.js
@@ -1,11 +1,14 @@
 const navigation = document.querySelector('.navigation');
+const navigationSide = document.querySelector('.navigation-side');
 const btnOpen = document.querySelector('.navigation__open');
 const btnClose = document.querySelector('.navigation__close');
 
 btnOpen.addEventListener('click', () => {
     navigation.classList.add('navigation--active');
+    navigationSide.classList.add('navigation-side--active');
 })
 
 btnClose.addEventListener('click', () => {
     navigation.classList.remove('navigation--active');
+    navigationSide.classList.remove('navigation-side--active');
 })
