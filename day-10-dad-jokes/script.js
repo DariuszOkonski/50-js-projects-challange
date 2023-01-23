71f4f8e99a0ced2aad9280a448b03fbb24b82751
Dariusz Okonski <darek200180@gmail.com>
2023-01-24 00:32:03 +0100

    add js

--- day-10-dad-jokes/script.js
+++ day-10-dad-jokes/script.js
@@ -1,1 +1,30 @@
-console.log('Dad jokes')
+const URL = "https://icanhazdadjoke.com";
+const jokes = document.getElementById('jokes')
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => getData(URL, jokes));
+
+const getData = async(url, node) => {
+    let dataResponse = "";
+    
+    const config = {
+        headers: {
+            "Accept": "application/json"
+        }
+    }
+
+    try {
+        const response = await fetch(url, config);
+        if(response.status !== 200)
+            throw new Error();
+        
+            const data = await response.json();
+            dataResponse = data.joke;
+    } catch (error) {
+        dataResponse = 'Some error occured...'
+    }
+
+    node.innerText = dataResponse;
+}
+
+getData(URL, jokes);
