60459987010b518bc051d307f8719a9afb8aa352
Dariusz Okoński <darek200180@gmail.com>
2023-01-25 23:28:43 +0100

    working on js

--- day-12-faq-collapse/script.js
+++ day-12-faq-collapse/script.js
@@ -1,1 +1,7 @@
-console.log("frequently asked questions");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.parentElement.classList.toggle("active"));
+  });
+});
