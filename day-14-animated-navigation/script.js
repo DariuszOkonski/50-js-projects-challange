30b0f9fb467d541ff9934d817b79e585c7aa0ce6
Dariusz Okoński <darek200180@gmail.com>
2023-02-20 18:49:14 +0100

    add animated navigation

--- day-14-animated-navigation/script.js
+++ day-14-animated-navigation/script.js
@@ -1,6 +1,9 @@
-const navigation = document.getElementById("navigation");
 const btn = document.getElementById("btn");
+const navigation = document.querySelector("nav");
+
+console.log(btn);
 
 btn.addEventListener("click", () => {
+  console.log("Hello");
   navigation.classList.toggle("active");
 });
