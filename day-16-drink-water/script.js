346d38cd74620c182f2f4bd3396784c941ab20c6
Dariusz Okonski <darek200180@gmail.com>
2023-03-01 22:20:59 +0100

    add script

--- day-16-drink-water/script.js
+++ day-16-drink-water/script.js
@@ -1,1 +1,31 @@
-console.log('Hello world');
+const smallGlasses = document.querySelectorAll('.glass-small');
+const glassesContainer = document.querySelector('.glasses-container');
+let prevNumberOfGlasses = 0;
+let currentNumber = 0;
+
+glassesContainer.addEventListener('click', (e) => {
+    if(e.target.classList.contains('glass')) {
+        const howManyGlases = e.target.dataset.number
+        
+        if(prevNumberOfGlasses !== howManyGlases) {
+            emptyGlasses(smallGlasses, smallGlasses.length)
+            fillGlasses(smallGlasses, howManyGlases)
+            prevNumberOfGlasses = e.target.dataset.number
+        } else {
+            const currentGlass = smallGlasses[howManyGlases];
+            currentGlass.classList.toggle('active')
+        }        
+    }
+})
+
+function emptyGlasses(glasses, to) {
+    for (let index = 0; index < to; index++) {
+        glasses[index].classList.remove('active')        
+    }
+}
+
+function fillGlasses(glasses, to) {
+    for (let index = 0; index <= to; index++) {
+        glasses[index].classList.add('active')        
+    }
+}
