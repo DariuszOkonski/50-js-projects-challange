5921c30fc0eced0ab72dd4b791ddc8304c256955
Dariusz Okoński <darek200180@gmail.com>
2023-07-26 21:04:22 +0200

    add styles

---
+++ day-17-movie-app/script.js
@@ -0,0 +1,5 @@
+const API_URL =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=837f0db187c4a7cf9ceefc157b184b50&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=837f0db187c4a7cf9ceefc157b184b50&query="';
